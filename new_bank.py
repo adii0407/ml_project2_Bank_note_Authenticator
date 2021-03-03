b9d7a11f184ba3333401d0cf8c909be5866a253d
adii0407 <78608201+adii0407@users.noreply.github.com>
2021-03-03 23:51:11 +0530

    flask and flasgger files

---
+++ new_bank.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 23:52:41 2021
+
+@author: Aditya bhosle
+"""
+
+from flask import Flask,request
+import pandas as pd
+import numpy as np
+import pickle 
+
+
+app=Flask(__name__)
+pickle_in=open('classifier_1.pkl','rb')
+classifier=pickle.load(pickle_in)
+
+@app.route('/')
+def welcome():
+    return 'WELCOME ALL'
+@app.route('/predict')
+def predict_note_authentication():
+    variance=request.args.get('variance')
+    skewness=request.args.get('skewness')
+    curtosis=request.args.get('curtosis')
+    entropy=request.args.get('entropy')
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    return "the predicted value is"+ str(prediction)
+
+
+@app.route('/predict_file',methods=["POST"])
+def predict_note_file():
+    df_test=pd.read_csv(request.files.get("file"))
+    prediction=classifier.predict(df_test)
+    return "the predicted values for the csv is"+ str(list(prediction))
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5000)
